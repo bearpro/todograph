f2532d870ac0515453690f7a06b75966db452ed0
mprokazin <bearpro@outlook.com>
2026-06-02 15:08:00 +0300

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Elm + Bun + MongoDB development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+            };
+          });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Elm
+            elmPackages.elm
+            elmPackages.elm-format
+            elmPackages.elm-test
+            elmPackages.elm-review
+
+            # Bun backend
+            bun
+          ];
+
+          shellHook = ''
+            echo "elm: $(elm --version)"
+            echo "bun: $(bun --version)"
+          '';
+        };
+      });
+    };
+}
